6815b57d92325ca3b7c0b6cdfdba9e8b3adcc1fb
Jennifer Slaughter <129903316+j-slaughter@users.noreply.github.com>
2025-02-09 18:11:28 -0800

    add featured products route

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -3,6 +3,7 @@
  * @description Controller for handling products
  */
 import Product from '../models/product.model.js';
+import { redis } from '../db/redis.js';
 
 /**
  * getAllProducts - retrieves all the products from the db
@@ -13,5 +14,35 @@ export const getAllProducts = async (req, res) => {
     return res.status(200).json({ products, message: 'Retrieved all products' });
   } catch (error) {
     return res.status(500).json({ message: `Error retrieving all products: ${error.message}` });
+  }
+};
+
+/**
+ * getFeaturedProducts - retrieves all the featured products from the cache/db
+ */
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    // First, try to retrieve products from cache
+    let products = await redis.get('featured_products');
+    if (products) {
+      return res.status(200).json({
+        products: JSON.parse(products),
+        message: 'Retrieved all featured products from cache',
+      });
+    }
+    // Otherwise, retrieve products from db in plain JS object form
+    products = await Product.find({ isFeatured: true }).lean();
+    if (products.length > 0) {
+      // Save in cache
+      await redis.set('featured_products', JSON.stringify(products));
+      return res.status(200).json({ products, message: 'Retrieved all featured products from db' });
+    } else {
+      // 404 (Not found)
+      return res.status(404).json({ message: 'No featured products found!' });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `Error retrieving featured products: ${error.message}` });
   }
 };
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -5,10 +5,12 @@
 
 import express from 'express';
 import { verifyAccess, adminAccess } from '../middleware/auth.middleware.js';
-import { getAllProducts } from '../controllers/product.controller.js';
+import { getAllProducts, getFeaturedProducts } from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 router.get('/', verifyAccess, adminAccess, getAllProducts);
+
+router.get('/featured', getFeaturedProducts);
 
 export default router;
