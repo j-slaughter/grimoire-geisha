e2de485cd899bff3730c8f2cc68ba5a9e9aece75
Jennifer Slaughter <129903316+j-slaughter@users.noreply.github.com>
2025-03-17 17:42:49 -0700

    ignore react prop types linting

--- eslint.config.js
+++ eslint.config.js
@@ -29,6 +29,7 @@ export default [
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/prop-types': 'off',
     },
   },
 ];
