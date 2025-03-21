fe48e40ce54062a6e2c516015e69f2d41ff6a7d7
Jennifer Slaughter <129903316+j-slaughter@users.noreply.github.com>
2025-03-20 17:25:30 -0700

    create forms for new products and coupons

--- client/src/components/CouponForm.jsx
+++ client/src/components/CouponForm.jsx
@@ -3,17 +3,117 @@
  * @description Coupon form tab component for admin dashboard
  */
 
+import { useState } from 'react';
+import { TicketPlus, Loader } from 'lucide-react';
+import { motion } from 'motion/react';
+
 function CouponForm() {
+  // Store new coupon form info
+  const [newCoupon, setNewCoupon] = useState({
+    code: '',
+    discountPercentage: '',
+    expirationDate: '',
+  });
+
+  // Format date for expiration date minimum
+  const formatDate = () => {
+    const date = new Date();
+    // Convert the date to ISO string
+    const isoString = date.toISOString();
+    // Split at the "T" character to get the date part
+    const formattedDate = isoString.split('T')[0];
+    return formattedDate;
+  };
+
+  const todayDate = formatDate();
+
+  /**
+   * handleSubmit - handles coupon form data to create new coupon
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newCoupon);
+  };
+
+  const loading = false;
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="relative">
-        <div className="w-20 h-20 border-emerald-200 border-2 rounded-full"></div>
-        <div className="w-20 h-20 border-emerald-500 border-t-2 animate-spin rounded-full absolute left-0 top-0"></div>
-        <div className="mt-5 text-emerald-500 text-center text-xl font-semibold animate-pulse">
-          Coupon Form...
+    <motion.div
+      className="bg-gray-800 shadow-lg rounded-lg p-8 mb-8 max-w-xl mx-auto"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+    >
+      <h2 className="text-2xl text-center font-semibold font-audiowide mb-6 text-emerald-300">
+        Create A New Coupon
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+          <label htmlFor="code" className="block text-sm font-medium text-gray-300">
+            Coupon Code
+          </label>
+          <input
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            type="text"
+            id="code"
+            name="code"
+            value={newCoupon.code}
+            onChange={(e) => setNewCoupon({ ...newCoupon, code: e.target.value })}
+            required
+          />
         </div>
-      </div>
-    </div>
+        <div className="mb-2">
+          <label htmlFor="discountPercentage" className="block text-sm font-medium text-gray-300">
+            Discount Percentage
+          </label>
+          <input
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            type="number"
+            step="1"
+            min="0"
+            max="100"
+            id="discountPercentage"
+            name="discountPercentage"
+            placeholder="0"
+            value={newCoupon.discountPercentage}
+            onChange={(e) => setNewCoupon({ ...newCoupon, discountPercentage: e.target.value })}
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="expirationDate" className="block text-sm font-medium text-gray-300">
+            Expiration Date
+          </label>
+          <input
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            type="date"
+            min={todayDate}
+            id="expirationDate"
+            name="expirationDate"
+            value={newCoupon.expirationDate}
+            onChange={(e) => setNewCoupon({ ...newCoupon, expirationDate: e.target.value })}
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={loading}
+          className="w-full flex justify-center items-center py-2 px-4 rounded-md shadow-sm disabled:opacity-50"
+        >
+          {loading ? (
+            <>
+              <Loader className="mr-2 h-5 w-5 animate-spin" aria-hidden="true" />
+              Loading...
+            </>
+          ) : (
+            <>
+              <TicketPlus className="mr-2 h-5 w-5" />
+              Create Coupon
+            </>
+          )}
+        </button>
+      </form>
+    </motion.div>
   );
 }
 
--- client/src/components/ProductForm.jsx
+++ client/src/components/ProductForm.jsx
@@ -19,6 +19,34 @@ function ProductForm() {
     image: '',
   });
 
+  /**
+   * handleImageUpload - handles upload of new product image
+   */
+  const handleImageUpload = (e) => {
+    // Get image file uploaded by admin
+    const file = e.target.files[0];
+    if (file) {
+      // Create file reader
+      const reader = new FileReader();
+      // Once loaded, add image to state
+      reader.onloadend = () => {
+        setNewProduct({ ...newProduct, image: reader.result });
+      };
+      // Convert to base64 encoded string
+      reader.readAsDataURL(file);
+    }
+  };
+
+  /**
+   * handleSubmit - handles product form data to create new product
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newProduct);
+  };
+
+  const loading = false;
+
   return (
     <motion.div
       className="bg-gray-800 shadow-lg rounded-lg p-8 mb-8 max-w-xl mx-auto"
@@ -26,9 +54,116 @@ function ProductForm() {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
     >
-      <h2 className="text-2xl text-center font-semibold mb-6 text-emerald-300">
+      <h2 className="text-2xl text-center font-semibold font-audiowide mb-6 text-emerald-300">
         Create A New Product
       </h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+          <label htmlFor="name" className="block text-sm font-medium text-gray-300">
+            Product Name
+          </label>
+          <input
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            type="text"
+            id="name"
+            name="name"
+            value={newProduct.name}
+            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+            required
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="price" className="block text-sm font-medium text-gray-300">
+            Price
+          </label>
+          <input
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            type="number"
+            step="0.01"
+            min="0"
+            id="price"
+            name="price"
+            placeholder="0.00"
+            value={newProduct.price}
+            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+            required
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="description" className="block text-sm font-medium text-gray-300">
+            Description
+          </label>
+          <textarea
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            rows="3"
+            id="description"
+            name="description"
+            value={newProduct.description}
+            onChange={(e) => setNewProduct({ ...newProduct, description: e.target.value })}
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="category" className="block text-sm font-medium text-gray-300">
+            Category
+          </label>
+          <select
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            id="category"
+            name="category"
+            value={newProduct.category}
+            onChange={(e) => setNewProduct({ ...newProduct, category: e.target.value })}
+            required
+          >
+            <option value="">Select a category</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-5 flex items-center">
+          <input
+            type="file"
+            id="image"
+            className="sr-only"
+            accept="image/*"
+            onChange={handleImageUpload}
+          />
+          <label
+            htmlFor="image"
+            className="cursor-pointer bg-gray-700 py-2 px-3 border border-gray-600 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-300 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-emerald-500"
+          >
+            <Upload className="inline-block" size={18} />
+            <span className="inline ml-2">Upload Image</span>
+          </label>
+          {newProduct.image && (
+            <>
+              <span className="ml-3 text-sm text-gray-400">Image Uploaded!</span>
+              {/* Image Preview*/}
+              <img src={newProduct.image} className="h-20 w-20 ml-2" />
+            </>
+          )}
+        </div>
+        <button
+          type="submit"
+          disabled={loading}
+          className="w-full flex justify-center items-center py-2 px-4 rounded-md shadow-sm disabled:opacity-50"
+        >
+          {loading ? (
+            <>
+              <Loader className="mr-2 h-5 w-5 animate-spin" aria-hidden="true" />
+              Loading...
+            </>
+          ) : (
+            <>
+              <CirclePlus className="mr-2 h-5 w-5" />
+              Create Product
+            </>
+          )}
+        </button>
+      </form>
     </motion.div>
   );
 }
